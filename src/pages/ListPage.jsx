67701a287e71f9924f696499d83456c1f412a487
Marco Mechini <mechinimarco35@gmail.com>
2025-01-27 17:32:27 +0100

    feat: chiamata api per prendere i dati dei film

--- src/pages/ListPage.jsx
+++ src/pages/ListPage.jsx
@@ -1,11 +1,34 @@
-import { useState } from "react"
+import axios from "axios"
+import { useEffect, useState } from "react"
 
 function ListPage() {
-    const [count, setCount] = useState(0)
+
+    const apiUrl = import.meta.env.VITE_API_URL
+    const [movieList, setMovieList] = useState(0)
+
+    useEffect(() => {
+        getPost()
+    }, [])
+
+    const getPost = () => {
+        console.log(apiUrl);
+
+        axios.get(`${apiUrl}/movies`).then(resp => {
+            setMovieList(resp.data)
+        }
+        )
+    }
 
     return (
         <>
             <h1>post</h1>
+            <ul>
+                {movieList && movieList.map((curMovie) => (
+                    <li key={curMovie.id}>
+                        <span>{curMovie.title}</span>
+                    </li>
+                ))}
+            </ul>
         </>
     )
 }
