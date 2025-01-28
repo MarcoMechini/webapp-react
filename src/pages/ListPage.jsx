a2c6c209d28014d7ac0623c23e51073f1eff8eca
Marco Mechini <mechinimarco35@gmail.com>
2025-01-28 16:44:12 +0100

    feat: aggiunti filtri per generi

--- src/pages/ListPage.jsx
+++ src/pages/ListPage.jsx
@@ -7,16 +7,24 @@ function ListPage() {
     const apiUrl = import.meta.env.VITE_API_URL
     const [movieList, setMovieList] = useState([])
     const [search, setSearch] = useState("")
+    const [selectedGenre, setSelectedGenre] = useState("")
+
+    // fare chiamata api per prenderli
+    const genres = ['Science Fiction', 'Crime'];
 
     useEffect(() => {
         getPost()
-    }, [])
+    }, [selectedGenre])
 
     const getPost = () => {
 
         const params = {};
         if (search.length > 0) {
             params.search = search;
+        }
+
+        if (selectedGenre !== "") {
+            params.genre = selectedGenre
         }
 
         axios.get(`${apiUrl}/movies`, { params }).then(resp => {
@@ -28,6 +36,13 @@ function ListPage() {
     return (
         <>
             <section>
+                <select name="" id="" value={selectedGenre} onChange={(e) => setSelectedGenre(e.target.value)}>
+                    <option value="">Tutti</option>
+                    {genres.map((curGenre, index) => (
+                        <option key={index} value={curGenre}>
+                            {curGenre}
+                        </option>))}
+                </select>
                 <input
                     value={search}
                     onChange={(e) => setSearch(e.target.value)}
